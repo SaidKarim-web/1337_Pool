27ffbe0d27ba503e96b2b96a102951739d783432
saim <98237419+SaidKarim-web@users.noreply.github.com>
2023-08-16 18:24:40 +0100

    Update r_dwssap.sh

--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d ':' -f 1 | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | tr '\n' '.'
+cat /etc/passwd | grep -v '^#' | awk 'NR % 2 == 0' | cut -d ':' -f 1 | rev | sort -r | awk 'NR >= ENVIRON["FT_LINE1"] && NR <= ENVIRON["FT_LINE2"]' | tr '\n' ', ' | sed 's/, $/./'
