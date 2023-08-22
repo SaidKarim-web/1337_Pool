acf62f0f75d9ee955a1942c4adc1bc92d149c583
SaidKarim-web <sirsaidkarim@gmail.com>
2023-08-22 15:27:10 +0100

    ex07 modification

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/17 14:22:37 by skarim            #+#    #+#             */
-/*   Updated: 2023/08/17 15:06:59 by skarim           ###   ########.fr       */
+/*   Created: 2023/08/22 15:26:23 by skarim            #+#    #+#             */
+/*   Updated: 2023/08/22 15:26:29 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ void	ft_putnbr(int nb)
 		ft_putchar(nb + '0');
 	else if (nb < 0)
 	{
-		ft_putchar('-');
-		ft_putchar(nb * (-1) + '0');
+		if (nb == -2147483648)
+			write(1, "-2147483648", 11);
+		else
+		{
+			ft_putchar('-');
+			ft_putnbr(nb * (-1));
+		}
 	}
 	else
 	{
