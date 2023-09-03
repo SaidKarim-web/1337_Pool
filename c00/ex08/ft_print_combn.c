477fec6bae04a1b87ee8db49f716010340a218d8
SaidKarim-web <sirsaidkarim@gmail.com>
2023-09-03 18:39:14 +0100

    modify ex07 and adding ex08

--- c00/ex08/ft_print_combn.c
+++ c00/ex08/ft_print_combn.c
@@ -5,39 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/01 21:41:11 by skarim            #+#    #+#             */
-/*   Updated: 2023/09/01 21:50:33 by skarim           ###   ########.fr       */
+/*   Created: 2023/09/02 13:10:25 by skarim            #+#    #+#             */
+/*   Updated: 2023/09/02 14:57:08 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int	ft_checklast(char *tab, int n)
 {
-	write(1, &c, 1);
+	int	i;
+
+	i = n - 1;
+	while (i >= 0)
+	{
+		if (tab[i] != '9' - (n - 1 - i))
+			return (0);
+		i--;
+	}
+	return (1);
 }
 
-void	ft_putnbr(int nb)
+void	ft_print_combn(int n)
 {
-	long	n;
+	char	tab[9];
+	int		i;
 
-	n = nb;
-	if (n < 0)
+	i = -1;
+	while (++i < n)
+		tab[i] = '0' + i;
+	while (1)
 	{
-		ft_putchar('-');
-		n = -n;
-	}
-	if (n < 10)
-		ft_putchar(n + '0');
-	else
-	{
-		ft_putnbr(n / 10);
-		ft_putchar(n % 10 + '0');
+		i = 0;
+		while (i < n)
+			write(1, &tab[i++], 1);
+		if (ft_checklast(tab, n))
+			return ;
+		i = n - 1;
+		while (i >= 0 && tab[i] == '9' - (n - 1 - i))
+			i--;
+		tab[i]++;
+		i++;
+		while (i < n)
+		{
+			tab[i] = tab[i - 1] + 1;
+			i++;
+		}
+		write(1, ", ", 2);
 	}
 }
 /*
 int	main(void)
 {
-	ft_putnbr(-2147483648);
+	ft_print_combn(2);
 	return (0);
 }*/
