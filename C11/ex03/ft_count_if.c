f655df647a1a6603144fcfcf2193abc85111f664
SaidKarim-web <sirsaidkarim@gmail.com>
2023-09-06 23:05:32 +0100

    adding ex03

---
+++ C11/ex03/ft_count_if.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skarim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 23:50:07 by skarim            #+#    #+#             */
+/*   Updated: 2023/09/06 09:26:39 by skarim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+int	ft_search(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == 'A')
+			return (1);
+		i++;
+	}
+	return (0);
+}*/
+int	ft_count_if(char **tab, int length, int (*f)(char*))
+{
+	int	i;
+	int	count;
+
+	count = 0;
+	i = 0;
+	while (i < 3)
+	{
+		if (f(tab[i]))
+			count++;
+		i++;
+	}
+	return (count);
+}
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	*tab[] = {"sesg", "slelA", "esAel"};
+
+	printf("%d", ft_count_if(tab, 3, &ft_search));
+	return (0);
+}*/
